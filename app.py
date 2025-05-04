3ffc4e787d9a90676d1f75f248bf8d504fcf3dd7
Ankita Ghosh <ankitaghosh9978@gmail.com>
2025-05-04 08:39:43 +0530

    Updated for Render deployment

--- app.py
+++ app.py
@@ -114,4 +114,5 @@ def download_file(filename):
 
 # ✅ REQUIRED: Add this to run the server
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
