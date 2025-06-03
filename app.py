3e889879f619a49f49fd237fe8ea7331735eda3c
Ankita Ghosh <ankitaghosh9978@gmail.com>
2025-06-03 12:32:10 +0530

    maow maow

--- app.py
+++ app.py
@@ -117,3 +117,6 @@ def uploads():
     conn.close()
     return render_template('uploads.html', logs=logs)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or fallback to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
