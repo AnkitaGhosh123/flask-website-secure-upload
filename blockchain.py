d8fbfe78d86e54fb0c2b3720e158cd3a35ab17a7
Ankita Ghosh <ankitaghosh9978@gmail.com>
2025-06-06 13:27:21 +0530

    u

--- blockchain.py
+++ blockchain.py
@@ -1,29 +1,43 @@
 import hashlib
-import time
+import os
+import datetime
 
 class Block:
-    def __init__(self, index, data, prev_hash):
+    def __init__(self, index, timestamp, data, previous_hash):
         self.index = index
-        self.timestamp = time.time()
+        self.timestamp = timestamp
         self.data = data
-        self.prev_hash = prev_hash
+        self.previous_hash = previous_hash
         self.hash = self.calculate_hash()
 
-    def calculate_hash(self, filepath):
-    try:
-        with open(filepath, 'rb') as f:
-            content = f.read()
-        return hashlib.sha256(content).hexdigest()
-    except Exception:
-        return None
+    def calculate_hash(self):
+        value = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
+        return hashlib.sha256(value.encode()).hexdigest()
 
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
-        return Block(0, "Genesis Block", "0")
+        return Block(0, str(datetime.datetime.now()), "Genesis Block", "0")
 
-    def add_block(self, block):
-        if block.prev_hash == self.chain[-1].hash:
-            self.chain.append(block)
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        latest_block = self.get_latest_block()
+        new_block = Block(
+            index=latest_block.index + 1,
+            timestamp=str(datetime.datetime.now()),
+            data=data,
+            previous_hash=latest_block.hash
+        )
+        self.chain.append(new_block)
+
+    def calculate_hash(self, filepath):
+        try:
+            with open(filepath, 'rb') as f:
+                content = f.read()
+            return hashlib.sha256(content).hexdigest()
+        except Exception:
+            return None
